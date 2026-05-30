ddc2c35a6b015cee704cc90b3f09e29eec72a220
pranathi <pranathireddyserigudem@gmail.com>
2026-05-30 21:15:17 +0530

    added files

---
+++ module-3/q15.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class q15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(new StringBuilder(str).reverse());
+    }
+}
---
+++ module-3/q19.java
@@ -0,0 +1,25 @@
+interface Playable {
+    void play();
+}
+
+class Guitar implements Playable {
+    public void play() {
+        System.out.println("Playing Guitar");
+    }
+}
+
+class Piano implements Playable {
+    public void play() {
+        System.out.println("Playing Piano");
+    }
+}
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Guitar guitar = new Guitar();
+        Piano piano = new Piano();
+
+        guitar.play();
+        piano.play();
+    }
+}
