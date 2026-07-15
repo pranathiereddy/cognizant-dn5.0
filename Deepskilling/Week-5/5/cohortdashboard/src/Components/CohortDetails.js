7a5229fa2ffa2dcc19bd8d313a6f33a717efcf2a
pranathi <pranathireddyserigudem@gmail.com>
2026-07-15 09:33:27 +0530

    added files

---
+++ Deepskilling/Week-5/5/cohortdashboard/src/Components/CohortDetails.js
@@ -0,0 +1,43 @@
+import styles from "./CohortDetails.module.css";
+import React from "react";
+
+
+function CohortDetails({ cohort }) {
+
+    return (
+
+        <div className={styles.box}>
+
+            <h3
+                className={
+                    cohort.currentStatus === "Ongoing"
+                        ? styles.ongoing
+                        : styles.completed
+                }
+            >
+                {cohort.cohortCode}
+            </h3>
+
+            <dl>
+
+                <dt>Started On</dt>
+                <dd>{cohort.startedOn}</dd>
+
+                <dt>Current Status</dt>
+                <dd>{cohort.currentStatus}</dd>
+
+                <dt>Coach</dt>
+                <dd>{cohort.coach}</dd>
+
+                <dt>Trainer</dt>
+                <dd>{cohort.trainer}</dd>
+
+            </dl>
+
+        </div>
+
+    );
+
+}
+
+export default CohortDetails;
